daf40a3dd8ae9a677017a8ab2ef03eed2d639bdc
APCS2nd <APCS2nd@MHS-Math2-19>
2018-09-10 09:57:21 -0700

    This is beginning calculate library

---
+++ Access Calculate/src/DoMath.java
@@ -0,0 +1,12 @@
+/* This is the client code for the Calculate Library
+ * @author Celeste Liu
+ * @version September 6, 2018
+ */
+public class DoMath {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ Calculate Library/src/Calculate.java
@@ -0,0 +1,10 @@
+/* This class contains the methods to do various math related tasks.
+ * @author Celeste Liu
+ * @version September 6, 2018
+ */
+public class Calculate {
+
+	public int square(int number) {
+		return number*number;
+	}
+}
