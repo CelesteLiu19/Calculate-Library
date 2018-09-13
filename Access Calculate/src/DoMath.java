0e0a0051b92fa7014fbbf460240929cad95a60e8
liuce <liuce@MSI>
2018-09-13 09:49:32 -0700

    starting methods

--- Access Calculate/src/DoMath.java
+++ Access Calculate/src/DoMath.java
@@ -5,8 +5,30 @@
 public class DoMath {
 
 	public static void main(String[] args) {
+		System.out.println(Calculate.square(-3));
+	
+		System.out.println(Calculate.cube(-3));
 		
-
+		System.out.println(Calculate.average(6,2));
+		
+		System.out.println(Calculate.average(6,2,1));
+		
+		System.out.println(Calculate.toDegrees(1));
+				
+		System.out.println(Calculate.toRadians
+		
+		System.out.println(Calculate.
+		
+		System.out.println(Calculate.
+		
+		System.out.println(Calculate.
+		
+		System.out.println(Calculate.
+		
+		System.out.println(Calculate.
+		
+		System.out.println(Calculate.
+		
+		
 	}
-
 }
--- Calculate Library/src/Calculate.java
+++ Calculate Library/src/Calculate.java
@@ -4,7 +4,31 @@
  */
 public class Calculate {
 
-	public int square(int number) {
+	public static int square(int number) {
 		return number*number;
 	}
+	public static int cube(int number) {
+		return number*number*number;
+	}
+	public static double average(double number1, double number2) {
+		return (number1+number2)/2;
+	}
+	public static double average(double number1, double number2, double number3) {
+		return (number1+number2+number3)/3;
+	}
+	public static double toDegrees(double x) {
+		return(x*180/3.14159);
+	}
+	public static double toRadians(double x) {
+		return(x*3.14159/180);
+	}
+	public static double 
+	
+	
+	
+	
+	
+	
+	
+	
 }
