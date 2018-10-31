574de2c556c0cbc4e275238ec6f6b5ea855d22cf
APCS2nd <APCS2nd@MHS-Math2-19>
2018-10-31 11:37:37 -0700

    part one done

--- Sandwiches/src/Split.java
+++ Sandwiches/src/Split.java
@@ -4,7 +4,12 @@ public class Split {
  
 
  public static void main(String[] args) {
-
+	partZero();
+	partOne();
+	
+ }
+ 
+ public static void partZero() {
 // Your task Part 0
 
 //String.split();
@@ -17,17 +22,22 @@ public class Split {
 
 // Example: "I like apples!".split(" ");
 	System.out.println(Arrays.toString("I like apples!".split(" ")));
-	
 // it will split at spaces and return an array of ["I","like","apples!"]
 
 // Example 2: "I really like really red apples"split("really")
-																																																																																																																																																																																																																																																																																													
+	System.out.println(Arrays.toString("I really like really red apples".split("really")));
 // it will split at the word "really" and return an array of ["I "," like ","red apples!"]
 
 //play around with String.split!
 
 //What happens if you "I reallyreally likeapples".split("really") ?
-
+	System.out.println(Arrays.toString("I reallyreally like apples".split("really")));
+	String[] applesArray = "I reallyreally like apples".split("really");
+	System.out.println(Arrays.toString(applesArray));
+	System.out.println(applesArray[2]);
+ }
+ 
+ public static void partOne() {
 //Your task Part 1:
 
 /*Write a method that take in a string like
@@ -37,13 +47,24 @@ public class Split {
 * describing a sandwich.
 
 * Use String.split to split up the sandwich by the word "bread" and return what's in the middle of
-
 * the sandwich and ignores what's on the outside
-
 * What if it's a fancy sandwich with multiple pieces of bread?
 
 */
-
+	
+	String[] sandwichArray = "applespineapplesbreadlettustomatobaconmayohambreadcheese".split("bread");
+	//System.out.println(Arrays.toString(sandwichArray));
+	//	System.out.println(sandwichArray[1]);
+		
+		
+	
+		String sandwich = "applespineapplesbreadlettustomatobaconmayohambreadcheese";
+		int indexBread =sandwich.indexOf("bread");
+		sandwich = sandwich.substring(indexBread);
+		String[] noBread = sandwich.split("bread");
+		System.out.println(Arrays.toString(noBread));
+		System.out.println(noBread[1]);
+	//	
 //Your task pt 2:
 
 /*Write a method that take in a string like
